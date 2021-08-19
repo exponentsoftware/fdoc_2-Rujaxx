b31ee493f65fc0b2b640381a2b32bf021d0ca7bd
Rujaxx <shaikhrujan@gmail.com>
2021-08-20 05:08:11 +0530

    day2 complete

--- second.js
+++ second.js
@@ -43,12 +43,54 @@ const users = [
 	}
 	];
 
-const scoreFilter= (users)=>{
-    let result = users.filter((score)=> score.scores >= 85)
-    return result
+// const scoreFilter= (users)=>{
+//     let result = users.filter((score)=> score.scores >= 85)
+//     return result
+// }
+
+// console.log(scoreFilter(users))
+
+
+const addUser =(user)=>{
+	let User = users.find(User => User.name === user.name);
+	if(!User){
+		users.push(user);
+	}else{
+		console.log('user  exists')
+	}
 }
 
-console.log(scoreFilter(users))
+let user = {name:"Rujan",scores:90,skills:['HTMl', 'CSS', 'JS'],age:22};
+	addUser(user);
+console.log(users)
 
+const addUserSkill =(user)=>{
+	let User = users.find(User => User.name === user.name);
+	if(User){
+		User.skills.push(user.skills);
+        console.log('added')
+	}else{
+		console.log('user does not exist')
+	}
+}
 
+var user = {name:"Thomas",skills:'sa'};
+	addUserSkill(user);
+console.log(users)
 
+const editUser =(user)=>{
+	let User = users.find(User => User.name === user.name);
+	if(User){
+		User.skills.push(user.skills);
+        User.age=user.age
+        User.scores=user.scores
+//       User.skills=user.skills
+        console.log('added')
+	}else{
+		console.log('user does not exist')
+	}
+}
+
+var user = {name:"Thomas",skills:'sa',age:22,scores:55};
+	editUser(user);
+console.log(users)
